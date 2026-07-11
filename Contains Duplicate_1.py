ecd47b0761f2c50af4f9f1dc71564578694ad88d
andy <andy89102919@gmail.com>
2026-07-11 13:27:09 +0800

    feat: add hasDuplicate solution using sort

---
+++ Contains Duplicate_1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        def quick(arr):
+            if len(arr) <= 1:
+                return arr
+            pivot = arr[len(arr) // 2]
+            left = [x for x in arr if x < pivot]
+            mid = [x for x in arr if x == pivot]
+            right = [x for x in arr if x > pivot]
+            return quick(left) + mid + quick(right)
+        sort = quick(nums)
+        for i in range (len(sort)-1):
+            if sort[i] == sort[i+1]:
+                return True
+        return False
