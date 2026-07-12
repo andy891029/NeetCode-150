d7c8cb772f4d5d68bc0b38d4ab3e73518342a776
andy <andy89102919@gmail.com>
2026-07-12 20:31:10 +0800

    12

--- Contains Duplicate_1.py
+++ Contains Duplicate_1.py
@@ -14,5 +14,63 @@ class Solution:
             if sort[i] == sort[i+1]:
                 return True
         return False
-data = [4,32,12,5,22,23,22,17,42,9]
-print(Solution().hasDuplicate(data))
+
+data = [5,34,46,1,22,21,27,64,43,22,19]
+def merge(left,right):
+    final = []
+    i = 0;j = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            final.append(left[i])
+            i += 1 
+        else: 
+            final.append(right[j])
+            j += 1
+    final.extend(left[i:])
+    final.extend(right[j:])
+    return final
+def merge_sort(arr):
+    if len(arr) <=1:
+        return arr
+    mid = len(arr)//2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left,right)
+#print(merge_sort(data))
+
+class Solution:
+    def hasDuplicate(self, nums: list[int]) -> bool:
+        def merge(left,right):
+            final = []
+            i = 0;j = 0
+            while i < len(left) and j < len(right):
+                if left[i] <= right[j]:
+                    final.append(left[i])
+                    i += 1 
+                else: 
+                    final.append(right[j])
+                    j += 1
+            final.extend(left[i:])
+            final.extend(right[j:])
+            return final
+        def merge_sort(arr):
+            if len(arr) <=1:
+                return arr
+            mid = len(arr)//2
+            left = merge_sort(arr[:mid])
+            right = merge_sort(arr[mid:])
+            return merge(left,right)
+        sort = merge_sort(nums)
+        for i in range (len(sort)-1):
+            if sort[i] == sort[i+1]:
+                return True
+        return False
+class Solution:
+    def hasDuplicate(self, nums: list[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+#print(Solution().hasDuplicate(data))
