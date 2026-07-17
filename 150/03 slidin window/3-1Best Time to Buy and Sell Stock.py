02029c97f77333886167207303925907b6fabdde
andy <andy89102919@gmail.com>
2026-07-17 16:23:05 +0800

    0717

--- 150/03 slidin window/3-1Best Time to Buy and Sell Stock.py
+++ 150/03 slidin window/3-1Best Time to Buy and Sell Stock.py
@@ -1,3 +1,14 @@
 #prices = [10,1,5,6,7,1]
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
+        money = 0
+        left = 0;right = 1
+        while right < len(prices):
+            if prices[left] > prices[right]:
+                left = right 
+            else:
+                profit = prices[right] - prices[left]
+                money = max(money,profit)
+            right += 1
+        return money
+#print(Solution().maxProfit(prices))
