aacca06cfcd0903858048c2a8faa9f690571b8f5
andy <andy89102919@gmail.com>
2026-07-20 18:23:53 +0800

    0720

--- 150/04 stack/4-1Valid Parentheses.py
+++ 150/04 stack/4-1Valid Parentheses.py
@@ -1,2 +1,28 @@
+#s = "([{}])"
 class Solution:
+    
     def isValid(self, s: str) -> bool:
+        if len(s)%2:
+            return False
+        def decode(char) :
+            if char == "]": return "["
+            if char == ")": return "("
+            if char == "}": return "{"
+        """
+        decode = {")": "(","]": "[","}": "{"}
+        """
+        stack = []
+        for char in s:
+            if char in "({[":
+                stack.append(char)
+            else :
+                if len(stack) == 0:
+                    return False
+                if stack[-1] == decode(char):
+                    stack.pop()
+                else:
+                    return False
+        return len(stack) == 0 #not stack
+    
+
+#print(Solution().isValid(s))
