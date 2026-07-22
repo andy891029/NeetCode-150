1e9357baab0237112b409a31a849c2bd657574a0
andy <andy89102919@gmail.com>
2026-07-22 17:54:00 +0800

    0722

--- 150/04 stack/4-3Evaluate Reverse Polish Notation.py
+++ 150/04 stack/4-3Evaluate Reverse Polish Notation.py
@@ -1,2 +1,36 @@
+#tokens = ["1","2","+","3","*","4","-"]
+'''
 class Solution:
     def evalRPN(self, tokens: list[str]) -> int:
+        stack = []
+        operators = {"+","-","*","/"}
+        for char in tokens:
+            if char not in operators:
+                stack.append(int(char))
+            else:
+                ans = int(eval(f"{stack[-2]}{char}{stack[-1]}"))
+                del stack[-1]
+                stack[-1] = ans
+        return stack[-1]
+'''        
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+        stack = []
+        operators = {"+","-","*","/"}
+        for char in tokens:
+            if char not in operators:
+                stack.append(int(char))
+            else:
+                right = stack.pop();left = stack.pop()
+                if char == "+":
+                    ans = left + right
+                elif char == "-":
+                    ans = left - right
+                elif char == "*":
+                    ans = left * right
+                else:
+                    ans = int(left/right)
+                stack.append(ans)
+
+        return stack[-1]
+#print(Solution().evalRPN(tokens))
