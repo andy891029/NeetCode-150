32b792ef33f23d655a4adc893b573196d1920645
andy <andy89102919@gmail.com>
2026-07-23 20:31:19 +0800

    0723

--- 150/04 stack/4-6Largest Rectangle In Histogram(沒寫完).py
+++ 150/04 stack/4-6Largest Rectangle In Histogram(沒寫完).py
@@ -1,3 +1,4 @@
+heights = [7,1,7,2,2,4]
 class Solution:
     def largestRectangleArea(self, heights: list[int]) -> int:
         
---
+++ 150/05 Binary Search/5-1Binary Search.py
@@ -0,0 +1,4 @@
+nums = [-1,0,2,4,6,8]; target = 4
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        
